e54155d7fc4c7b6f55490d3bf9125bb404430526
Amith Raravi <amith.raravi@gmail.com>
2022-10-15 14:26:06 +0200

    added css file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
+import './App.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
